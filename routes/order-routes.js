be4707c194d085eee9e23e0b4f6493f0fe74832a
shabnam <shabnammosavi2020@gmail.com>
2024-12-13 11:46:41 +0330

    'add'

---
+++ routes/order-routes.js
@@ -0,0 +1,10 @@
+const express=require('express')
+
+const orderController=require('../controllers/order-controller')
+
+
+const router=express.Router()
+
+router.post('/',orderController.createOrder)
+
+module.exports=router
